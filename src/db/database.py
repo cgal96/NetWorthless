222aaf536b28489d3d76029630a70db9f2d3e3c2
CGal <conorgalvincregane@hotmail.com>
2026-01-08 19:02:01 +0000

    Adding initial file structure and boilerplate for db file.

---
+++ src/db/database.py
@@ -0,0 +1,21 @@
+# src/db/database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "sqlite:///networthless.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True
+)
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False,
+    future=True
+)
+
+Base = declarative_base()
